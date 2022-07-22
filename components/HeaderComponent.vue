6c20dcf2c118da8c7c077090e4b5b4190cee0e85
FEkimhyoeun <hyoeun.kim@flitto.com>
2022-07-22 17:01:04 +0900

    feat: header 컴포넌트 반응형 추가

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -42,6 +42,10 @@ header {
   justify-content: space-between;
   align-items: center;
   padding: 1em;
+  @media (max-width: 580px) {
+    flex-direction: column;
+    justify-content: center;
+  }
 }
 .header-left {
   display: flex;
@@ -57,11 +61,14 @@ header {
   font-size: 1.75em;
   color: $black;
   padding-top: 0.25em;
+  @include tablet {
+    display: none;
+  }
 }
 .header-right {
   display: flex;
   flex-direction: row;
-  width: 35vw;
+  width: 25em;
   justify-content: space-between;
 }
 .header-right__search-input {
@@ -69,20 +76,21 @@ header {
   border-radius: 1em;
   width: 18.75em;
   height: 3.125em;
+  padding-left: 1em;
+  color: $black;
 }
-.header-right__search-input {
-  padding-left: 1rem;
+.header-right__search-icon {
   color: $black;
 }
 .profile-wrapper {
   padding-left: 2em;
+  position: relative;
 }
 .profile-wrapper__profile-img {
   width: 2.5em;
   height: 2.5em;
   background-color: $deep-gray;
   border-radius: 5em;
-  position: relative;
 }
 .profile-wrapper__alert {
   width: 0.625em;
@@ -90,7 +98,7 @@ header {
   background-color: $normal-blue;
   border-radius: 0.625em;
   position: absolute;
-  top: 1.75rem;
-  right: 1rem;
+  top: -0.1em;
+  right: -0.1em;
 }
 </style>

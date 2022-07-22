3e3357185917dc7ba256a7bb492882145cb9957d
kim-dokyung <dokyung.kim@flitto.com>
2022-07-22 17:29:19 +0900

    feat: AfterLogin 컴포넌트 진행 중, 알림 기능 추가 필요

---
+++ components/dropdown/AfterLoginDropdown.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="after-login">
+    <header class="after-login__header">
+      <div class="after-login__header-left">
+        <h2><strong class="user-name">플리토</strong> 님</h2>
+        <!-- 유저 이름 들어가야 함 -->
+        <p>안녕하세요!</p>
+      </div>
+      <div class="after-login__header-right">
+        <button type="button">
+          <img
+            class="after-login__exit"
+            src="@/assets/svg/login/exit.svg"
+            alt="exit"
+          />
+        </button>
+        <button type="button" class="after-login__logout">로그아웃</button>
+      </div>
+    </header>
+    <nav>
+      <ul>
+        <li class="nav__link"><NuxtLink to="/mypage">마이페이지</NuxtLink></li>
+        <li class="nav__link">
+          <NuxtLink to="/contenteditpage">새 글 쓰기</NuxtLink>
+        </li>
+        <li class="nav__link">
+          <a href="https://www.flitto.com/careers">인재 채용</a>
+        </li>
+        <li class="nav__link">
+          <NuxtLink to="/notificationpage">알림</NuxtLink>
+          <button>
+            <img src="@/assets/svg/login/down.svg" alt="show_notification" />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AfterLoginDropdown',
+}
+</script>
+
+<style lang="scss" scoped>
+.after-login {
+  width: 25rem;
+  padding: 2rem 0;
+  border-radius: 1.5rem;
+  border: solid $black 0.1rem;
+}
+
+.after-login__header {
+  height: 5rem;
+  padding: 0rem 2rem 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+}
+
+.user-name {
+  color: $normal-blue;
+}
+
+.after-login__header-left {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.after-login__logout {
+  width: 6rem;
+  height: 1.75rem;
+  font-size: 1rem;
+  background-color: $normal-gray;
+  border-radius: 0.5rem;
+
+  &:hover {
+    background-color: $normal-blue;
+  }
+}
+
+.after-login__header-right {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+
+.after-login__exit {
+  width: 1.25rem;
+}
+
+.nav__link {
+  width: 100%;
+  height: 3em;
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font: {
+    size: 1.5em;
+    weight: 500;
+  }
+
+  &:hover {
+    background-color: $normal-gray;
+  }
+}
+
+.nav__link--dropdown-tag {
+}
+</style>

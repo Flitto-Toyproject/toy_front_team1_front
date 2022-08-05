9c8694465eeb2e45e2c74e41ac8090b78943161a
FEkimhyoeun <hyoeun.kim@flitto.com>
2022-08-05 16:05:06 +0900

    feat: GET post api 연결

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: `${process.env.baseUR}/api/`,
+  baseURL: 'http://localhost:33000/api/',
 })
 
 // auth
--- store/index.js
+++ store/index.js
@@ -1,6 +1,22 @@
+// constants
+import { getPosting } from '@/api'
+export const GET_POSTS = 'GET_POSTS'
+
 // store
-export const state = () => ({})
+export const state = () => ({
+  posts: [],
+})
 
-export const mutations = {}
+export const mutations = {
+  getPosts(state, newPosts) {
+    state.posts = [...newPosts]
+  },
+}
 
-export const actions = {}
+export const actions = {
+  async [GET_POSTS]({ commit }) {
+    const { data } = await getPosting()
+    const posts = [...data.post_list]
+    commit('getPosts', posts)
+  },
+}

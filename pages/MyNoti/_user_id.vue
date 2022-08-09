baed6c98484e14df73851025d678fede7c640fd4
kim-dokyung <dokyung.kim@flitto.com>
2022-08-09 17:09:54 +0900

    feat: 알림 페이지 퍼블리싱 * 서버에서부터 받은 알림 데이터의 날짜를 현재 날짜와 비교해서 오늘, 어제, 그 이전 알림으로 구분(date-fns) * 각 날짜별로 알림을 구분하기 위해 비어있는 객체 사용

---
+++ pages/MyNoti/_user_id.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="mynoti">
+    <div class="mynoti__wrap">
+      <div class="mynoti__header">
+        <TitleBasic :title-content="'Notification'" />
+        <div class="read-all" @click="readAllNoti">모두 읽음 처리</div>
+      </div>
+
+      <div
+        v-for="(notifications, key) in notiPerDays"
+        :key="key"
+        class="mynoti__per-day"
+      >
+        <div class="mynoti__day">{{ key }}</div>
+        <div class="noti-wrap">
+          <div
+            v-for="noti in notifications"
+            :key="noti.notification_id"
+            class="noti"
+          >
+            <div class="noti__message" v-html="noti.message"></div>
+            <div class="noti__time">{{ noti.created_at }}</div>
+            <div v-if="noti.read" class="notification-alert"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { formatRelative } from 'date-fns'
+import TitleBasic from '@/components/basic/TitleBasic.vue'
+import { notiDataArr } from '@/api/test.js'
+
+export default {
+  name: 'MyNoti',
+  components: { TitleBasic },
+  asyncData() {
+    const notiPerDays = {}
+    const dayExpression = { today: '오늘', yesterday: '어제' }
+    const today = new Date()
+
+    notiDataArr.forEach((noti) => {
+      const day = noti.created_at.split(' ')[0]
+      const relativeDay = formatRelative(new Date(day), today).split(' at')[0]
+      const dayKey = dayExpression[relativeDay] ?? day
+
+      const value = notiPerDays[dayKey]
+      notiPerDays[dayKey] = value ? [...value, noti] : [noti]
+    })
+
+    return { notiPerDays }
+  },
+  methods: {
+    readAllNoti() {
+      console.log('read all notifications')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.mynoti {
+  display: flex;
+  justify-content: center;
+}
+
+.mynoti__wrap {
+  width: 42rem;
+}
+
+.mynoti__header {
+  margin-bottom: 1rem;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+.read-all {
+  padding: 0;
+  text-decoration: none;
+  cursor: pointer;
+  font: {
+    size: 0.8rem;
+    weight: 500;
+  }
+
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+}
+
+.mynoti__per-day {
+  margin-bottom: 1rem;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+.mynoti__day {
+  color: $light-blue;
+  font: {
+    size: 1.1rem;
+    weight: 600;
+  }
+}
+
+.noti-wrap {
+  position: relative;
+  width: 60%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.noti {
+  padding: 0.5rem 1rem 0.5rem 1rem;
+  font-size: 0.8rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  cursor: pointer;
+  &:hover {
+    background-color: $light-gray;
+  }
+}
+
+.noti__message {
+  padding-bottom: 0.5rem;
+}
+
+.noti__time {
+  color: $deep-gray;
+}
+
+.notification-alert {
+  width: 0.35em;
+  height: 0.35em;
+  background-color: $normal-blue;
+  border-radius: 0.35em;
+
+  position: absolute;
+  top: 1.5rem;
+  right: -1.5rem;
+}
+</style>
